3cb145d835e4f99518bcfe40372aa4622b573319
Erin Callison <36282047+ecallison@users.noreply.github.com>
2018-06-29 14:40:33 -0700

    Add files via upload

---
+++ Updated the navigation bar to display user-s name instead of their e-mail address/HomeController.cs
@@ -0,0 +1,18 @@
+   public class HomeController : Controller
+    {
+        //Generate User's first and last name in navigation bar (see _LoginPartial.cshtml file)
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (User != null)
+            {
+                var context = new ApplicationDbContext();
+                var username = User.Identity.Name;
+                if (!string.IsNullOrEmpty(username))
+                {
+                    var user = context.Users.SingleOrDefault(u => u.UserName == username);
+                    string fullName = string.Concat(new string[] { user.FirstName, " ", user.LastName });
+                    ViewData.Add("FullName", fullName);
+                }
+            }
+            base.OnActionExecuted(filterContext);
+        }
